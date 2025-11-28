99aa08e84ae4c938bd9c0e7d65d73c2442daad32
Emie Duha Nakişci <emirduha93@gmail.com>
2025-11-28 19:30:06 +0300

    first commit

---
+++ Assets/Scripts/General/DayManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DayManager : MonoBehaviour
+{
+
+    [SerializeField] private int firstTargetFloor = 10;
+    [SerializeField] private int targetFloorIncrement = 5;
+    int currentTargetFloor;
+    int currentDay = 1;
+
+    public void StartNewDay()
+    {
+        currentTargetFloor = firstTargetFloor + (currentDay - 1) * targetFloorIncrement;
+        Debug.Log("Starting to day " + currentDay + "with target floor " + currentTargetFloor);
+    }
+
+    public void CompleteDay()
+    {
+        Debug.Log("Day " + currentDay + " completed!");
+        currentDay++;
+        StartNewDay();
+    }
+}
