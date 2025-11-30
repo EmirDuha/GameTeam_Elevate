a3dae1bb8687b431ffa830e9ea74ad4156e11e38
durusaakiii <durukurek047@gmail.com>
2025-11-30 18:44:54 +0300

    emergencybuttonactivated

---
+++ Assets/sesefektleriveacildurum/emergencybuttonmanager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    // Bu metot, Unity Editör'de Button bileşeninin OnClick() olayına atanacaktır.
+    public void PrintMessageToConsole()
+    {
+        // İstediğiniz mesajı buraya yazın:
+        Debug.Log("Butona tıklandı! emergency button is activated.");
+    }
+}
